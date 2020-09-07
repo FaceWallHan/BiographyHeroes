e65e8993afe7a5c7ad5b463729669f71323cf074
FaceWallHan <1428528183@qq.com>
2020-09-07 22:24:29 +0800

    想通过继承viewgroup来实现scrollview，乱！

--- app/src/main/java/com/example/hero/MainActivity.java
+++ app/src/main/java/com/example/hero/MainActivity.java
@@ -13,19 +13,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TopBar topBar=findViewById(R.id.topBar);
-        topBar.setTopBarClickListener(new TopBar.topBarClickListener() {
-            @Override
-            public void leftClick() {
-                Toast.makeText(MainActivity.this, "leftClick", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void rightClick() {
-                Toast.makeText(MainActivity.this, "rightClick", Toast.LENGTH_SHORT).show();
-            }
-        });
+        setContentView(R.layout.scrollview_layout);
+//        setContentView(R.layout.activity_main);
+//        TopBar topBar=findViewById(R.id.topBar);
+//        topBar.setTopBarClickListener(new TopBar.topBarClickListener() {
+//            @Override
+//            public void leftClick() {
+//                Toast.makeText(MainActivity.this, "leftClick", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void rightClick() {
+//                Toast.makeText(MainActivity.this, "rightClick", Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
     
 }
